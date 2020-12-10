31c1d6ebcc682af3534522c39a524d31e74f5da1
ThuyVan <thuyvan121998@gmail.com>
2020-12-10 17:14:55 +0700

    module-test

---
+++ Test/Block/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace AHT\Test\Block;
+  
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+  
+class Index extends Template
+{
+    
+}
---
+++ Test/Controller/Index/Index.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace AHT\Test\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class Index extends Action {
+   
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+
+}
+
+
+
